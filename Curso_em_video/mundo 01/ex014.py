4143acb6efac664494afad8fb7b5eb33d24df9d5
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2025-04-25 01:21:10 -0300

    create ex014

---
+++ Curso_em_video/mundo 01/ex014.py
@@ -0,0 +1,5 @@
+# Leia uma temperatura em graus Celsius e converta em graus fahrenheit
+
+c = float(input('Temperatura em graus c° Celcius: '))
+f = (c * 1.8) + 32
+print(f'{c}° graus Ceucius são {f}° graus fahrenheit')
