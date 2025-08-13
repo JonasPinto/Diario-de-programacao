6329b4b585d7f52a03a1b5017561e4ef60dd71d9
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2025-08-13 13:46:29 -0300

    Delete ex010.py

--- Curso_em_video/mundo 01/ex010.py
+++
@@ -1,9 +0,0 @@
-# crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dolares ela pode comprar
-
-import time
-
-r = float(input('Qantos reais voce tem na carteira : '))
-d = r / 5.80
-print('\n\nTROCANDO O DINHEIRO AGUARDE............... \n')
-time.sleep(6)
-print(f'Com ${r} reais você compra US${d:.2f} dolares')
