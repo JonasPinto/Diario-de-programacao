06da4efe73d1c8aab9900d527189922e90198cb9
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2025-08-13 13:46:46 -0300

    Delete ex015.py

--- Curso_em_video/mundo 01/ex015.py
+++
@@ -1,7 +0,0 @@
-#  Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.
-
-print(f'_' * 40,'\n\n KM     = R$0,15\n', 'Diaria = R$60,00\n', '_' * 40)
-qtd_km = float(input('\nInforme a quantidade de KM rodados: ').replace(',', '.')) * 0.15
-qtd_dias = int(input('Informe a quantidade de dias de uso : ')) * 60
-print(f'O Total a pagar é de R${qtd_km + qtd_dias:.2f}')
-
