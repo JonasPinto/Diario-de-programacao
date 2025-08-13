3c41838ddeca76474c85533b5b394e9f7e0b8721
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2025-08-13 19:37:00 -0300

    Create zer@dengue.cpp

---
+++ ADS/1° semestre/PTI 1°semestre/zer@dengue.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <iostream>
+
+
+
+using namespace std;
+ 
+int main(){
+
+
+int op = 0; 
+
+
+
+  cout <<"-----------------------------------------\n\n2";
+  cout <<"             ZER@DENGUE             \n\n";
+  cout <<"SEJA BEM VINDO ESCOLHA UMA OPCAO A SEGUIR\n\n"; 
+  cout <<"----------------------------------------\n";
+
+ system("pause");
+ system("cls");
+
+ printf("[1] FAZER LOGIN\n");
+ printf("[2] FAZER NOVO CADASTRO\n");
+ scanf("%i",&op);
+
+system("cls");
+
+switch (op)
+{
+case 1:
+    
+    break;
+case 2:
+
+    break;
+
+default:
+   printf("OPCAO INVALIDA");
+    break;
+}
+
+  
+  
+return 0;
+}
+
