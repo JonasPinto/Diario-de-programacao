f4c338c1558960b5a6b19b189c346f9afe700f25
jonas pinto <jonaspinto.ti@outlook.com>
2025-08-22 12:12:09 -0300

    create ex023

---
+++ Udemy/python_language/exercicios_seção_6/ex023.py
@@ -0,0 +1,15 @@
+"""
+Faça um algoritmo que leia um número positivo e emprima seus divisores
+"""
+# Algoritmo para ler um número positivo e imprimir seus divisores
+
+# Lê o número
+n = int(input("Digite um número positivo: "))
+
+if n > 0:
+    print(f"Divisores de {n}:")
+    for i in range(1, n + 1):
+        if n % i == 0:
+            print(i)
+else:
+    print("Por favor, digite um número positivo.")
